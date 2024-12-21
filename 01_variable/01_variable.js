d0aeeacf4ccc62e99071d68765ff8a6e96564ab9
Bappy Modok <bappymodok0011@gmail.com>
2024-12-21 11:23:06 +0600

    learn variabels

---
+++ 01_variable/01_variable.js
@@ -0,0 +1,10 @@
+const accountId = '12345';
+let email = "bappymodok0011@gmail.com";
+var password  = 'bappy123';
+
+//  accountId = 2233; not change constend variables
+
+email = "bappy@gmail.com"; 
+password = "11111"
+
+console.table([accountId,email,password])
